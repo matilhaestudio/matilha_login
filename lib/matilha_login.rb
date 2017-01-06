09e5fce74eec097eef338cee7c52bdce9dfd86f4
Diego Haas Sanches <egoforia@gmail.com>
2017-01-06 17:59:23 -0200

    configuração para a engine configurar as variáveis do facebook no devise

--- lib/matilha_login.rb
+++ lib/matilha_login.rb
@@ -3,4 +3,29 @@ require "omniauth-facebook"
 require "matilha_login/engine"
 
 module MatilhaLogin
+	mattr_accessor :facebook_app_id
+	mattr_accessor :facebook_app_secret
+
+	def self.facebook_app_id=(app_id)
+		@@facebook_app_id = app_id
+		config_omniauth
+	end
+
+	def self.facebook_app_secret=(app_secret)
+		@@facebook_app_secret = app_secret
+		config_omniauth
+	end
+
+	def self.setup
+		yield self
+	end
+
+	private
+		def self.config_omniauth
+			return if MatilhaLogin.facebook_app_id.blank? or MatilhaLogin.facebook_app_secret.nil?
+			Devise.setup do |config|
+				# setting up facebook omniauth configuration
+	  			config.omniauth :facebook, MatilhaLogin.facebook_app_id, MatilhaLogin.facebook_app_secret
+			end
+		end
 end
---
+++ test/dummy/config/initializers/matilha_login.rb
@@ -0,0 +1,5 @@
+MatilhaLogin.setup do |config|
+	config.facebook_app_id = "1660887170876551"
+	config.facebook_app_secret = "3dcd72e8ab7fda6d2591fbc31edbc3cb"
+end
+
